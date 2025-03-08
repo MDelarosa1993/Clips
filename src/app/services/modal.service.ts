0f3289511bcf1611bd94708ae851d4e67a3a3762
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-08 13:46:11 -0500

    Created modal interface to use in the modal service

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
-
+import { IModal } from '../interfaces/imodal';
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
-
   constructor() { }
 }
