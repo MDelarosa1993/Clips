127f8044f6c07c930683425d7cfab649cf142268
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-08 20:29:14 -0500

    Generated the Tabs and Tab components using the CLI

---
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs-container',
+  imports: [],
+  templateUrl: './tabs-container.component.html',
+  styleUrl: './tabs-container.component.css'
+})
+export class TabsContainerComponent {
+
+}
