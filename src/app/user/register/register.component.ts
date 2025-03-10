4fe8c6c5d33073c8084143645bcb211f931a30a5
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-09 21:33:42 -0400

    Created a reusable input component

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { InputComponent } from '../../shared/input/input.component';
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, InputComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
@@ -11,7 +12,7 @@ export class RegisterComponent {
   fb = inject(FormBuilder);
 
   form = this.fb.nonNullable.group({
-    name: ['', [Validators.required]],
+    name: ['', [Validators.required, Validators.minLength(3)]],
     email: [''],
     age: [18],
     password: [''],
