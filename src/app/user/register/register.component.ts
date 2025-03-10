5f16a5095b6250b5e01978ff4993b818928f463f
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-09 20:18:16 -0400

    Added the fields to the register form

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  fb = inject(FormBuilder);
 
+  form = this.fb.nonNullable.group({
+    name: [''],
+    email: [''],
+    age: [18],
+    password: [''],
+    confirmPassword: [''],
+    phoneNumber: [''],
+  });
 }
