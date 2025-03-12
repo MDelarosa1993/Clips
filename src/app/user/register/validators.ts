6adfa92dd1ceafb2dac24efae499c98fb09d40a1
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-11 21:55:14 -0400

    Added an error for the passwords not matching and outputting an error message

--- src/app/user/register/validators.ts
+++ src/app/user/register/validators.ts
@@ -1,17 +1,23 @@
 import { ValidationErrors, AbstractControl, ValidatorFn } from '@angular/forms';
 
-export function Match(controlName: string, matchingControlName: string): ValidatorFn {
-    return (group: AbstractControl): ValidationErrors | null => {
-        const control = group.get(controlName);
-        const matchingControl = group.get(matchingControlName);
-        if (!control || !matchingControl) {
-            return { controlNotFound: false };
-        }
-        const error =
-            control.value === matchingControl.value ? null: {
-                noMatch: true,
-            }
-        return error; 
+export function Match(
+  controlName: string,
+  matchingControlName: string
+): ValidatorFn {
+  return (group: AbstractControl): ValidationErrors | null => {
+    const control = group.get(controlName);
+    const matchingControl = group.get(matchingControlName);
+    if (!control || !matchingControl) {
+      console.error('Form controls can not be found in the FormGroup.')
+      return { controlNotFound: false };
     }
-  
+    const error =
+      control.value === matchingControl.value
+        ? null
+        : {
+            noMatch: true,
+          };
+    matchingControl.setErrors(error);
+    return error;
+  };
 }
