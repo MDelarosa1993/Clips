88c2ae6af6f7912e7cb9b88866a3789e12fc1f33
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-12 15:17:49 -0400

    Created an upload component, added the upload route with metadata to only let autheticated users to be in that route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { AboutComponent } from './views/about/about.component';
 import { ManageComponent } from './views/manage/manage.component';
+import { UploadComponent } from './views/upload/upload.component';
 
 export const routes: Routes = [
   {
@@ -19,4 +20,11 @@ export const routes: Routes = [
       authOnly: true
     }
   },
+  {
+    path: 'upload',
+    component: UploadComponent,
+    data: {
+      authOnly: true
+    }
+  }
 ];
