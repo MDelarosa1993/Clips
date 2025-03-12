0ffa6c0457c0e53f75bd43c3a7b56a04ab574145
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-12 11:25:21 -0400

    Created Manage component and added routing to the view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { AboutComponent } from './views/about/about.component';
+import { ManageComponent } from './views/manage/manage.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent,
-  }
+  },
+  {
+    path: 'manage',
+    component: ManageComponent,
+  },
 ];
