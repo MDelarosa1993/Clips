0b87c42f4c73886a924a041f2290b08169336fe2
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-13 11:20:38 -0400

    Created a directive to handle default behavior from an event and added it to the uploader

--- src/app/views/upload/upload.component.ts
+++ src/app/views/upload/upload.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { EventBlockerDirective } from '../../shared/directives/event-blocker.directive';
 @Component({
   selector: 'app-upload',
-  imports: [],
+  imports: [EventBlockerDirective],
   templateUrl: './upload.component.html',
   styleUrl: './upload.component.css'
 })
