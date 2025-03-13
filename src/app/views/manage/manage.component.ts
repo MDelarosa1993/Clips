e493f99ceb189b5925883c968f2cf4c8dd8024f7
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-13 09:02:37 -0400

    Created a clip component and started to set up query params for the clips and sorting

--- src/app/views/manage/manage.component.ts
+++ src/app/views/manage/manage.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterLink, Router, ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-manage',
   imports: [RouterLink],
   templateUrl: './manage.component.html',
-  styleUrl: './manage.component.css'
+  styleUrl: './manage.component.css',
 })
-export class ManageComponent {
+export class ManageComponent implements OnInit {
+  router = inject(Router);
+  route = inject(ActivatedRoute);
+  videoOrder = signal('1');
+
+  sort($event: Event) {
+    const { value } = $event.target as HTMLSelectElement;
+    this.router.navigateByUrl(`/manage?sort=${value}`);
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.videoOrder.set(params['sort'] === '2' ? '2' : '1')
+    });
+  }
 }
