33957411d0fa9dd82a2ba63145665204ef1261f2
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-13 12:17:25 -0400

    Handling the drag events on our upload component' '

--- src/app/views/upload/upload.component.ts
+++ src/app/views/upload/upload.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { EventBlockerDirective } from '../../shared/directives/event-blocker.directive';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-upload',
-  imports: [EventBlockerDirective],
+  imports: [EventBlockerDirective, NgClass],
   templateUrl: './upload.component.html',
   styleUrl: './upload.component.css'
 })
 export class UploadComponent {
+  isDragover = signal(false);
 
+  storeFile($event: Event) {
+    this.isDragover.set(false);
+  }
 }
