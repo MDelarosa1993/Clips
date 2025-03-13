2ac69c25e276565988f4e6f7926e4460f056632b
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-13 15:23:46 -0400

    Added functionality to the uplad component to show the form when a video has been dropped

--- src/app/views/upload/upload.component.ts
+++ src/app/views/upload/upload.component.ts
@@ -1,19 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { EventBlockerDirective } from '../../shared/directives/event-blocker.directive';
 import { NgClass } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { InputComponent } from '../../shared/input/input.component';
+
 @Component({
   selector: 'app-upload',
-  imports: [EventBlockerDirective, NgClass],
+  imports: [EventBlockerDirective, NgClass, ReactiveFormsModule, InputComponent],
   templateUrl: './upload.component.html',
-  styleUrl: './upload.component.css'
+  styleUrl: './upload.component.css',
 })
 export class UploadComponent {
   isDragover = signal(false);
   file = signal<File | null>(null);
+  nextStep = signal(false);
+  fb = inject(FormBuilder);
+
+  form = this.fb.nonNullable.group({
+    title: ['', [Validators.required, Validators.minLength(3)]],
+  });
+
   storeFile($event: Event) {
     this.isDragover.set(false);
     this.file.set(($event as DragEvent).dataTransfer?.files.item(0) ?? null);
-    console.log(this.file())
+    if (this.file()?.type !== 'video/mp4') return;
+    this.form.controls.title.setValue(
+      this.file()?.name.replace(/\.[^/.]+$/, '') ?? ''
+    );
+    this.nextStep.set(true);
+    console.log(this.file());
   }
-  
+
+  uploadFile() {
+    console.log('File Uploaded!')
+  }
 }
