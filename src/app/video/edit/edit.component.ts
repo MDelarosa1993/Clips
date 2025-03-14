d13082a167a3ec5d3b5a320ff021528226eaf07f
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-14 13:10:25 -0400

    Binded the values from the video to the edit form to make it dynamic to the video being edited

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,12 +1,28 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, effect } from '@angular/core';
 import { ModalComponent } from '../../shared/modal/modal.component';
 import { Clip } from '../../interfaces/clip';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputComponent } from '../../shared/input/input.component';
+
 @Component({
   selector: 'app-edit',
-  imports: [ModalComponent],
+  imports: [ModalComponent, ReactiveFormsModule, InputComponent],
   templateUrl: './edit.component.html',
-  styleUrl: './edit.component.css'
+  styleUrl: './edit.component.css',
 })
 export class EditComponent {
   activeClip = input<Clip | null>(null);
+  fb = inject(FormBuilder);
+
+  form = this.fb.nonNullable.group({
+    id: [''],
+    title: ['', [Validators.required, Validators.minLength(3)]],
+  });
+
+  constructor() {
+    effect(() => {
+      this.form.controls.id.setValue(this.activeClip()?.docID ?? '');
+      this.form.controls.title.setValue(this.activeClip()?.title ?? '');
+    });
+  }
 }
