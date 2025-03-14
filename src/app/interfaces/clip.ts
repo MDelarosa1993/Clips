6f233a6b8ad628d9c3389301553463223e58399a
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-13 21:28:52 -0400

    Added a clips signal so I can store the videos and be able to loop through them and diplay them on my manage page

--- src/app/interfaces/clip.ts
+++ src/app/interfaces/clip.ts
@@ -1,10 +1,11 @@
 import { Timestamp } from "@angular/fire/firestore";
 
 export interface Clip {
+  docID?: string;
   uid: string;
   displayName: string;
   title: string;
   fileName: string;
   clipURL: string;
-  timestamp: Timestamp;
+  timestamp: Timestamp;  
 }
--- src/app/views/manage/manage.component.ts
+++ src/app/views/manage/manage.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink, Router, ActivatedRoute, Params } from '@angular/router';
 import { ClipService } from '../../services/clip.service';
+import { Clip } from '../../interfaces/clip';
 @Component({
   selector: 'app-manage',
   imports: [RouterLink],
@@ -12,22 +13,36 @@ export class ManageComponent implements OnInit {
   route = inject(ActivatedRoute);
   videoOrder = signal('1');
   clipService = inject(ClipService);
+  clips = signal<Clip[]>([]);
 
   sort($event: Event) {
     const { value } = $event.target as HTMLSelectElement;
     this.router.navigate([], {
       relativeTo: this.route,
       queryParams: {
-        sort: value
-      }
+        sort: value,
+      },
     });
   }
 
   async ngOnInit() {
     this.route.queryParams.subscribe((params: Params) => {
-      this.videoOrder.set(params['sort'] === '2' ? '2' : '1')
+      this.videoOrder.set(params['sort'] === '2' ? '2' : '1');
     });
     const results = await this.clipService.getUserClips();
-    console.log(results);
+    results.forEach((document) => {
+      const data = document.data();
+      this.clips.set([...this.clips(), 
+        {
+          docID: document.id,
+          uid: data['uid'],
+          displayName: data['displayName'],
+          title: data['title'],
+          timestamp: data['timestamp'],
+          fileName: data['fileName'],
+          clipURL: data['clipURL'],
+        }
+      ])
+    })
   }
 }
