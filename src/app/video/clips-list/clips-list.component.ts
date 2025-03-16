c62d98f10845f4ab9848a6b95aacb847d0df2ca9
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-15 21:31:52 -0400

    Created a Clip List comonent and added it to the Home Componenet

---
+++ src/app/video/clips-list/clips-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clips-list',
+  imports: [],
+  templateUrl: './clips-list.component.html',
+  styleUrl: './clips-list.component.css'
+})
+export class ClipsListComponent {
+
+}
