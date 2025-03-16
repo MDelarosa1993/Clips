7fb696426487967bc017616ed6a06d559106c749
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-15 22:11:56 -0400

    Creating a scroll point threshold to call the DB if reached

--- src/app/video/clips-list/clips-list.component.ts
+++ src/app/video/clips-list/clips-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-clips-list',
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './clips-list.component.html',
   styleUrl: './clips-list.component.css'
 })
-export class ClipsListComponent {
+export class ClipsListComponent implements OnInit, OnDestroy {
+
+  ngOnInit(){
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = () => {
+    const { scrollTop, offsetHeight } = document.documentElement;
+    const { innerHeight } = window;
+
+    const bottomOfWindow = Math.round(scrollTop) + innerHeight > offsetHeight - 150;
+    if(bottomOfWindow) {
+      console.log('Bottom!')
+    }
+  }
 
 }
