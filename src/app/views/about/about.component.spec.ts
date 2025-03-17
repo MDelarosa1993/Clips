c5c44fc5fb1cbabf4b297041aa16b51ea50c1249
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-17 15:40:19 -0400

    Created a test for the creation of the about page

--- src/app/views/about/about.component.spec.ts
+++ src/app/views/about/about.component.spec.ts
@@ -3,7 +3,7 @@ import { AboutComponent } from "./about.component";
 
 describe('About Component', () => {
   let fixture: ComponentFixture<AboutComponent>;
-
+  let component: AboutComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AboutComponent],
@@ -12,5 +12,11 @@ describe('About Component', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AboutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  })
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
   })
 });
