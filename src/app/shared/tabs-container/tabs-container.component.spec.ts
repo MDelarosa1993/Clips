ea54eef8099de0b6830e22baf1a69827c5f78b6c
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-17 16:14:50 -0400

    Created a dummy component to deal with nested components

---
+++ src/app/shared/tabs-container/tabs-container.component.spec.ts
@@ -0,0 +1,41 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TabsContainerComponent } from './tabs-container.component';
+import { Component } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: `
+  <app-tabs-container>
+    <app-tab tabTitle="Tab One">Hey!</app-tab>
+    <app-tab tabTitle="Tab Two">Hi!</app-tab>
+  </app-tabs-container>
+  `,
+  standalone: true,
+  imports: [TabsContainerComponent, TabComponent]
+})
+class TestPostComponent{}
+
+describe('TabsContainerComponent', () => {
+  let component: TestPostComponent;
+  let fixture: ComponentFixture<TestPostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TestPostComponent, TabsContainerComponent, TabComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestPostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have two tabs', () => {
+    const tabs = fixture.debugElement.queryAll(By.css('nav > a'));
+    expect(tabs.length).toBe(2)
+  })
+});
