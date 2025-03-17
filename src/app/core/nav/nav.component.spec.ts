0c851600f9aea63f30f8dadc6a0a64d062b2f0a4
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-17 17:04:06 -0400

    Testing the logout functionality in the nav component

--- src/app/core/nav/nav.component.spec.ts
+++ src/app/core/nav/nav.component.spec.ts
@@ -1,10 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavComponent } from './nav.component';
-import { of } from 'rxjs';
+import { firstValueFrom, of } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
 import { provideRouter } from '@angular/router';
 import { routes } from '../../app.routes';
-
+import { By } from '@angular/platform-browser';
 
 describe('NavComponent', () => {
   let component: NavComponent;
@@ -37,4 +37,14 @@ describe('NavComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should logout', () => {
+    const logoutButton = fixture.debugElement.query(By.css('button.px-2.cursor-pointer'));
+    
+    expect(logoutButton).withContext('not logged in').toBeTruthy();
+
+    logoutButton.triggerEventHandler('click');
+    const service = TestBed.inject(AuthService);
+    expect(service.logout).withContext('could not click the logout button').toHaveBeenCalledTimes(1);
+  })
 });
