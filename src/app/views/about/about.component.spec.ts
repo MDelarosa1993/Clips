e501ea1f06059ae1792af06999ab16f5552c78ac
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-03-17 09:50:10 -0400

    Created a new instance of the About component

--- src/app/views/about/about.component.spec.ts
+++ src/app/views/about/about.component.spec.ts
@@ -1,10 +1,16 @@
-import { TestBed } from "@angular/core/testing";
+import { TestBed, ComponentFixture } from "@angular/core/testing";
 import { AboutComponent } from "./about.component";
 
 describe('About Component', () => {
+  let fixture: ComponentFixture<AboutComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AboutComponent],
     }).compileComponents();
   });
-})
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AboutComponent);
+  })
+});
